cb5dff81806041218d2580389bb0e9e52d42611a
Ssamyyy <shauryasingh1040@gmail.com>
2026-04-02 23:01:58 +0530

    admi page env

--- frontend/app/admin/page.tsx
+++ frontend/app/admin/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 
-const API = process.env.NEXT_PUBLIC_API_URL2
+const API = process.env.NEXT_PUBLIC_API_URL2+"/api";
 
 // --- TYPES ---
 type TeamRole = "RED" | "BLUE";
